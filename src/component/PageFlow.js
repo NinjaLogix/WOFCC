889ec2dba36f518ab00e94757ca51159c70fdfcf
Unknown <branden.boyington@eagles.usm.edu>
2018-07-18 08:08:21 -0500

    Starting work on contact page

--- src/component/PageFlow.js
+++ src/component/PageFlow.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Landing from '../pages/Landing';
 import Services from '../pages/Services';
 import Directions from '../pages/Directions';
+import ContactUs from '../pages/ContactUs';
 
 class PageFlow extends React.Component{
     render(){
@@ -11,6 +12,7 @@ class PageFlow extends React.Component{
                 <Route exact path='/' component={Landing}/>
                 <Route path='/services' component={Services}/>
                 <Route path={'/directions'} component={Directions}/>
+                <Route path={'/contact-us'} component={ContactUs}/>
             </Switch>
         );
     }
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -4,8 +4,26 @@ import '../style/ContactUs.css';
 class ContactUs extends React.PureComponent{
     render(){
         return(
-            <div>
+            <div className={'contact-container'}>
+                <div className={'contact-header'}>
+                    <h1>Keep In Touch</h1>
+                    <h3>We love you! Come back anytime!</h3>
+                    <h2>FaceBook</h2>
+                    {/*TODO - image link is taking the whole width of the page instead of just the image.*/}
+                    <a href={'https://www.facebook.com/WordOfFaithSouthaven'}>
+                        <img className={'contact-social-badge'}
+                             src={require('../resources/freebies/Facebook_Home_logo_old.svg.png')}
+                        />
+                    </a>
+                    <h3>Some text goes here about facebook</h3>
+                </div>
+                <div className={'contact-left-bottom'}>
+                    <p>You have a concern, we have a care! It's all love around here! Let us know what's on your mind please!</p>
+                    {/*Image of pastor goes here*/}
+                </div>
+                <div className={'contact-right-bottom-form'}>
 
+                </div>
             </div>
         )
     }
