275b7803148624250f771e1d36cc0b51db67bcee
Branden Boyington <branden.boyington@autozone.com>
2018-10-09 00:09:18 -0500

    Working with DropBox api

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,8 +3,7 @@ import Fade from '@material-ui/core/Fade';
 import { connect } from 'react-redux';
 import { change_page } from "../redux-def/actions";
 import { Carousel } from 'react-bootstrap';
-import FILE_REGEX from '../script/appContext';
-import Dropbox from 'dropbox';
+import { FILE_REGEX, EXT_REGEX, ALT_REGEX, dropBox } from '../script/appContext';
 import '../style/wofcc_master.css';
 
 const mapDispatchToProps = dispatch =>{
@@ -30,39 +29,43 @@ class ConnectedLanding extends React.PureComponent{
         this.setState({page: 'landing'});
         this.props.change_page('landing');
 
-        const dropBox = new Dropbox.Dropbox({accessToken: process.env.REACT_APP_TOKEN});
         dropBox.filesListFolder({path: process.env.REACT_APP_CAROUSEL_PATH})
             .then(response => {
-                response.entries.map( thing => {
-                    //TODO - check if file matches the correct pattern ~ file_name-yyyymmdd-yyyymmdd.ext
-                    //if(FILE_REGEX.test(thing.path_display)){
-                        //TODO - split filename and get start and end dates
-                        //split file code goes here
-                        //let fileName =
-                        //let startDate =
-                        //let endDate =
-                        //TODO - check if today's date is within the file's run dates and if it is attempt to create a share link
-                        //if (today's date >= startDate and today's date <= endDate) {
+                response.entries.forEach( fileName => {
+                    let extIndex = fileName.name.indexOf('.');
+                    let ext = fileName.name.substring(extIndex);
+                    let file = fileName.name.substring(0, extIndex);
+                    let today = new Date();
+
+                    if((FILE_REGEX.test(file) || ALT_REGEX.test(file)) && EXT_REGEX.test(ext)){
+                        let pieces = file.split('-');
+                        let startDate = new Date(pieces[1]);
+                        let endDate = new Date(pieces[2]);
+
+                        if (today >= startDate && today < endDate) {
                             dropBox.sharingCreateSharedLinkWithSettings({
-                                path: thing.path_display,
+                                path: fileName.path_display,
                                 settings: {requested_visibility: {'.tag': 'public'}}
                             })
                                 .catch(error => console.error('Shared link error: ', error));
-                        //} elseif (today's date > endDate) {
-                        /*TODO - check if today's date is after the exp date and if it is then remove the share link
-                            dropBox.sharingUnshareFile({file: ''}) //full path of the file
-                                .then()
-                                .catch();
-                        */
-                        //}
-                    //}
-                    dropBox.sharingListSharedLinks({path: thing.path_display})
+                        }
+                    } else {
+                        console.log('Image naming not in correct format: ', fileName.name);
+                    }
+                    dropBox.sharingListSharedLinks({path: fileName.path_display})
                         .then(response => {
-                            response.links.map( innerThing => {
+                            response.links.forEach( innerThing => {
                                 if (innerThing['.tag'] === 'file') {
-                                    this.setState(prevState => ({
-                                        displayUrl:[...prevState.displayUrl, innerThing]
-                                    }))
+                                    let file = innerThing.name.substring(0, innerThing.name.indexOf('.'));
+                                    let startDate = new Date(file.split('-')[1]);
+                                    let endDate = new Date(file.split('-')[2]);
+                                    let today = new Date();
+
+                                    if ((today >= startDate && today < endDate) || (ALT_REGEX.test(file))) {
+                                        this.setState(prevState => ({
+                                            displayUrl: [...prevState.displayUrl, innerThing]
+                                        }))
+                                    }
                                 }
                             })
                         })
