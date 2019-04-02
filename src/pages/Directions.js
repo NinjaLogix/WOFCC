3a6d3a4504fbb64aa4b5ed88eed2b7b1635f2a34
Branden Boyington <branden.boyington@eagles.usm.edu>
2019-04-01 22:49:03 -0500

    Updated location and mailing address

--- src/pages/Directions.js
+++ src/pages/Directions.js
@@ -28,13 +28,17 @@ class ConnectedDirections extends React.PureComponent{
         return(
             <div className={'direction-container'}>
                 <h1>Trying to Find Us?</h1>
-                <h3>We aren't too hard to find. Currently located on North Creek Golf Course</h3>
+                <h2>We aren't too hard to find. Our building is located in suite D.</h2>
+                <h3>Mailing Address</h3>
+                <p>P.O. Box 1968 Southaven, MS 38671</p>
                 <div className={'directions-map-container'}>
-                    <Iframe url={'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1634.3650618322474!2d-90.07323101162912!3d34.988424078475646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x3157cf420a1fe2e8!2sWord+of+Faith+Southaven!5e0!3m2!1sen!2sus!4v1432622541051'} width={'850'}
-                            height={'400'}
-                            className={'directions-iframe-gmaps'}
-                            display={'initial'}
-                            allowFullScreen/>
+                    <Iframe
+                        url={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6540.728862544873!2d-90.02572996728493!3d34.94747358037176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87d5618baea2c68b%3A0xdb0f86cf9ad4538!2s1881+Nail+Rd+suite+d%2C+Horn+Lake%2C+MS+38637!5e0!3m2!1sen!2sus!4v1554175353512!5m2!1sen!2sus"}
+                        width={'850'}
+                        height={'400'}
+                        className={'directions-iframe-gmaps'}
+                        display={'initial'}
+                        allowFullScreen/>
                 </div>
             </div>
         )
