b15a3df9119e34993cd9ee025026f502e90947a2
Branden Boyington <branden.boyington@eagles.usm.edu>
2020-07-19 01:11:50 -0500

    fixed info card to not use dropbox

--- src/component/info/InfoCard.js
+++ src/component/info/InfoCard.js
@@ -1,42 +1,48 @@
-import React, {useState} from 'react';
-import {InfoWrapper, ImgSection, TextSection} from './InfoStyle';
-import {InfoModal} from './InfoModal';
-import {convertMarkdown} from '../../util';
-import {config} from '../../config/config'
+import React, { useState } from 'react';
+import { InfoWrapper, ImgSection, TextSection } from './InfoStyle';
+import { InfoModal } from './InfoModal';
+import { config } from '../../config/config';
+import { Typography } from '@material-ui/core';
 
-export const InfoCard = ({src, title, text, detail}) => {
-    const text_limit = 200;
-    const [useExpanded] = useState(text.length >= text_limit);
-    const [open, setOpen] = useState(false);
+export const InfoCard = ({ src, title, text, detail }) => {
+  const text_limit = 200;
+  const [useExpanded] = useState(text.length >= text_limit);
+  const [open, setOpen] = useState(false);
 
-    const concatText = text => `${text.substring(0, text_limit)}....`;
+  const concatText = text => `${text.substring(0, text_limit)}....`;
 
-    const handleClick = e => setOpen(!open)
+  const handleClick = e => setOpen(!open);
 
-    return (
-        <InfoWrapper 
-            elevation={3} 
-            onClick={handleClick}
-            expanded={useExpanded}
-            mobile={config.isMobile}>
+  const showText = () => {
+    if (!config.isMobile) {
+      return (<Typography variant={'subtitle2'}>{useExpanded ? concatText(text) : text}</Typography>);
+    }
+  };
 
-            {useExpanded && 
-                <InfoModal open={open} setOpen={setOpen} title={title} text={detail} img={src}/>
-            }
+  const allowDetail = () => {
+    if (config.isMobile || useExpanded){
+      return (<InfoModal open={open} setOpen={setOpen} title={title} text={detail ? detail : text} img={src}/>);
+    }
+  }
 
-            <ImgSection src={src}/>
+  return (
+    <InfoWrapper
+      elevation={3}
+      onClick={handleClick}
+      expanded={useExpanded}
+      mobile={config.isMobile}>
 
-            <TextSection>
-                <section>
-                    <h3>{title}</h3>
-                </section>
-                {useExpanded
-                    ?
-                        <section dangerouslySetInnerHTML={{__html: convertMarkdown(concatText(text))}}/>
-                    :
-                        <section dangerouslySetInnerHTML={{__html: convertMarkdown(text)}}/>
-                }
-            </TextSection>
-        </InfoWrapper>
-    )
-}
+      {allowDetail()}
+
+      <ImgSection src={src}/>
+
+      <TextSection>
+        <section>
+          <h3>{title}</h3>
+        </section>
+
+        {showText()}
+      </TextSection>
+    </InfoWrapper>
+  );
+};
